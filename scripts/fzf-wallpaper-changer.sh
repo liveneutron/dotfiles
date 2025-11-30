97b0315a0e10ffe60ef466ad9549617d97bf2ea9
Neutron <liveneutron@proton.com>
2025-11-30 10:45:53 +0530

    wallpaper-changer

---
+++ scripts/fzf-wallpaper-changer.sh
@@ -0,0 +1,10 @@
+cd ~/walls/ && 
+fzf -m --preview 'chafa {} -s 58 -c full' \
+    --bind 'enter:execute(
+      swww img {} \
+        --transition-type grow \
+        --transition-pos 0.95,0.95 \
+        --transition-duration 1.5 \
+        --transition-fps 144
+    )+abort' \
+    --bind 'ctrl-d:execute(rm {})' --header 'ctrl+d - del.'
